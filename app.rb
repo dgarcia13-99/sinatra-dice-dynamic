889d3eba4a4acd5742b6d263f9d60eb1cf4d23e1
Daniela Garcia <danielagarcia13.99@gmail.com>
2024-09-09 20:36:10 +0000

    made instance variable for @sides so that the entire path is fully dynamic

--- app.rb
+++ app.rb
@@ -56,10 +56,12 @@ end
 get("/dynamic/:number_of_dice/6") do
   @num_dice = params.fetch("number_of_dice").to_i
 
+  @sides = params.fetch("how_many_sides").to_i
+
   @rolls = []
 
   @num_dice.times do
-    die = rand(1..6)
+    die = rand(1..@sides)
 
     @rolls.push(die)
   end
